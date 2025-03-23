264b792015e3d1a194a24883b8f2a7eb88838547
Maksim Torzhkov <maxtorj@yandex.ru>
2025-03-23 14:34:29 +0300

    add generics tasks

---
+++ src/ru/yandex/practicum/polymorph/genericks/loanApp/AgeValidationRule.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.polymorph.genericks.loanApp;
+
+// Дополните класс для проверки возраста пользователя
+public class AgeValidationRule extends ValidationRule<Byte> {
+
+    public AgeValidationRule(Byte age) {
+        super(age, "Возраст для подачи на ипотеку должен быть старше 18 лет");
+    }
+
+    @Override
+    public boolean isValid() {
+        return value >= 18;
+    }
+}
